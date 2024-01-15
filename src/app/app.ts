49770d3ca8e7768bb6ef96092408d85d7b8fccbf
Loubout <louis.k.boutin@gmail.com>
2024-01-15 21:13:49 +0100

    start field shader

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,9 @@ import { Ticker } from '@pixi/ticker';
 import * as FILTERS from 'pixi-filters';
 import { AdvancedBloomFilter, KawaseBlurFilter } from 'pixi-filters';
 import * as PIXI from 'pixi.js';
-import { AlphaFilter, Container } from 'pixi.js';
+import { AlphaFilter, Container, Filter } from 'pixi.js';
 import { analyze } from 'web-audio-beat-detector';
-import { background, tapagesFrames } from '../assets/loader';
+import { tapagesFrames } from '../assets/loader';
 import { AudioCapture } from './audioCapture';
 
 interface MonkeyFrames {
@@ -12,12 +12,12 @@ interface MonkeyFrames {
 }
 
 const CONFIG = {
-    BEAT_ANIMATION_DURATION_MS: 200
+    BEAT_ANIMATION_DURATION_MS: 500,
+    BPM: 30,
 }
 
 // Prepare frames
 const monkeyFrames: MonkeyFrames = tapagesFrames;
-const backgroundFrame = background
 
 
 const bpmToFps = (bpmValue: number) => {
@@ -35,7 +35,7 @@ export class GameApp {
     }   
 
     private prepareBackgroundContainer(): Container {
-        const background: PIXI.Sprite = PIXI.Sprite.from(backgroundFrame.main);
+        const background: PIXI.Sprite = PIXI.Sprite.from('../assets/image/fff.png');
         background.anchor.set(0, 0);
         background.anchor.x = 0;
         background.anchor.y = 0;
@@ -43,33 +43,117 @@ export class GameApp {
         background.height = this.app.screen.height;
         const backgroundContainer = new Container();
         
-        const alphaFilter: AlphaFilter = new AlphaFilter(0.5)
+        const fragSrc = `
+        #define NUM_LAYERS 4.
 
+        precision highp float;
+        uniform vec3      iResolution;           // viewport resolution (in pixels)
+        uniform float     iTime;                 // shader playback time (in seconds)
+        uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+        varying vec2 vTextureCoord;
+        uniform sampler2D uSampler;
+
+        mat2 Rot(float a) {
+            float s = sin(a), c = cos(a);
+            return mat2(c, -s, s, c);
+        }
+
+        float Star(vec2 uv, float flare){
+            float d = length(uv);
+            float m = .05/d;
+            
+            float rays = max(0.,1.-abs(uv.x * uv.y * 1000.));
+            m += rays* flare;
+            uv *= Rot(3.1415/4.);
+            rays = max(0.,1.-abs(uv.x * uv.y * 1000.));
+            m += rays * .3 * flare;
+            m*= smoothstep(1.,.2,d);
+            return m;
+        }
+
+        //RNG
+        float Hash21(vec2 p){
+            p = fract(p*vec2(123.34, 456.21));
+            p += dot(p, p + 45.32);
+            return fract(p.x * p.y);
+        }
+
+        vec3 StarLayer(vec2 uv){
+            vec3 col = vec3(0);
+
+            //boxes
+            vec2 gv = fract(uv) - 0.5;
+            vec2 id = floor(uv);
+
+            for(int y=-1;y<=1;y++){
+                for(int x=-1;x<=1;x++){
+                    vec2 offs = vec2(x,y);
+                    float n=  Hash21(id+offs); // random betwen 0 and 1
+                    float size = fract(n*345.32);
+                    float star = Star(gv-offs-vec2(n,fract(n*34.))+.5,smoothstep(.8, .9, size));
+                    vec3 color = sin(vec3(.2,.3,.9)*fract(n*2345.2)*6.2831* 100.)* 0.5 + 0.5;
+                    color = color* vec3(1,.5,1.+size);            
+                    color += pow(length(gv - offs) * 0.1,mod(iTime + n,.2));
+                    star *= sin(iTime*90.+n*6.2831)*.2+1.;
+                    col += size * star * color;
+                }
+            }
+            return col;
+        }
+
+        void main(void)
+        {
+            // Normalized pixel coordinates (from -0.5 to 0.5)
+            vec2 uv = (gl_FragCoord.xy -.5*iResolution.xy)/iResolution.y;
+            vec2 M = (iMouse.xy -.5*iResolution.xy)/iResolution.y;
+            float t = iTime*.01;
+            uv += M *4.;
+            uv *= Rot(t);
+            vec3 col = vec3(0);
+            
+            for(float i=0.; i<1.; i+=1./NUM_LAYERS) {
+                float depth = fract(i+t);
+                float scale = mix(20.,.5,depth);
+                float fade = depth* smoothstep(1.,.9,depth);
+                col += StarLayer(uv*scale+i*453.2-M)*fade;
+            }
+            
+            //red grid
+            //if(gv.x > .48 || gv.y > .48) col.r = 1.;
+            
+            //col.rg += id*.4;
+            //col += Hash21(uv);
+            
+            // Output to screen
+            gl_FragColor = vec4(col,1.0);
+        }
+        `
+
+        const uniforms = {
+            iResolution: [this.app.screen.width, this.app.screen.height, 1],
+            iTime : 350,
+            iMouse: [this.app.screen.width*0.001, this.app.screen.height *0.4, this.app.screen.width*0.2, this.app.screen.height/2]
+        }
+        const starFilter = new Filter(undefined, fragSrc, uniforms)
         const blurFilter: KawaseBlurFilter = new KawaseBlurFilter(0.2, 3)
         const bloomFilter: AdvancedBloomFilter = new AdvancedBloomFilter({
             threshold: 0.5,
-            bloomScale: 2,
-            brightness: 1,
-            blur: 0,
+            bloomScale: 0.03,
+            brightness: 0.5,
+            blur: 0.8,
             quality: 4
         })
 
         var flip = false
         this.app.ticker.add(() => {
+            starFilter.uniforms.iTime += 0.05
+            // bloomFilter.bloomScale += 0.01
 
-            if (flip) {
-                bloomFilter.blur += 0.002
-                bloomFilter.brightness += 0.005
-            } else {
-                bloomFilter.blur -= 0.002
-                bloomFilter.brightness -= 0.005
-            }
-
-            if (bloomFilter.brightness > 1.8 || bloomFilter.brightness < 0.5) {
-                flip = !flip
-            }
+            // if (bloomFilter.bloomScale > .8) {
+            //     bloomFilter.bloomScale = 0.5
+            // }
         })
-        backgroundContainer.filters = [alphaFilter, bloomFilter, blurFilter]
+        backgroundContainer.filters = [starFilter, bloomFilter]
         backgroundContainer.addChild(background)
         return backgroundContainer
     }
@@ -79,8 +163,10 @@ export class GameApp {
         const outlineFilter = new FILTERS.OutlineFilter(2, 0x000000, 1)
 
         const monkey: PIXI.Sprite = PIXI.Sprite.from(monkeyFrames.main);
+
+
         const motionBlurFilter = new FILTERS.MotionBlurFilter([30,30], 15)
-        const RGCSplitFilter = new FILTERS.RGBSplitFilter([5,0], [7,0], [2,0])
+        const RGCSplitFilter = new FILTERS.RGBSplitFilter([11,0], [2,0], [1,4])
 
         const bpmFilters = [motionBlurFilter, RGCSplitFilter]
 
@@ -94,12 +180,12 @@ export class GameApp {
 
 
         this.app.ticker.add((delta) => {
-            monkey.rotation += .0005
+            monkey.rotation += .003
         })
-        const BPM = 80
+        
         const bpmTicker = new Ticker();
-        bpmTicker.minFPS = bpmToFps(BPM)
-        bpmTicker.maxFPS = bpmToFps(BPM)
+        bpmTicker.minFPS = bpmToFps(CONFIG.BPM)
+        bpmTicker.maxFPS = bpmToFps(CONFIG.BPM)
         bpmTicker.start();
 
         bpmTicker.add((time) => {
@@ -129,7 +215,7 @@ export class GameApp {
     private init() {
         const backgroundContainer = this.prepareBackgroundContainer();
         const logoContainer = this.prepareLogoContainer();
-        this.setupAudioCapture()
+        //this.setupAudioCapture()
         this.app.stage.addChild(backgroundContainer);
         this.app.stage.addChild(logoContainer);
     }
--- src/assets/loader.js
+++ src/assets/loader.js
@@ -1,12 +1,7 @@
 
 import tapagesLogo from './images/logo_tapages.webp';
-import backgroundImage from './images/susan-wilkinson.webp';
 
 
 export const tapagesFrames = {
     main: tapagesLogo,
-}
-
-export const background = {
-    main: backgroundImage,
 }
